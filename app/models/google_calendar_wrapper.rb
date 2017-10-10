04fce304fe0b80bdd06ba723cea5a957cff85916
emmanueldd <e.derozin@gmail.com>
2017-10-10 15:03:54 +0200

    update

--- app/models/google_calendar_wrapper.rb
+++ app/models/google_calendar_wrapper.rb
@@ -23,20 +23,20 @@ class GoogleCalendarWrapper
     calendar["id"]
   end
 
-  def add_event
-    event = {
-      summary: "Board of Directors Meeting",
-      location: "Conference Room - 1",
-      start: {dateTime: 2016-03-20T11:04:00+0000},
-      end: {dateTime: 2016-03-20T12:04:00+0000},
-      description: "important meeting with the board",
-    }
-    @client.execute(api_method: @service.events.insert,
-      parameters: {'calendarId': calendar_id,
-        'sendNotifications': true},
-      body: JSON.dump(event),
-      headers: {'Content-Type': 'application/json'}
-    )
-  end
+  # def add_event
+  #   event = {
+  #     summary: "Board of Directors Meeting",
+  #     location: "Conference Room - 1",
+  #     start: {dateTime: 2016-03-20T11:04:00+0000},
+  #     end: {dateTime: 2016-03-20T12:04:00+0000},
+  #     description: "important meeting with the board",
+  #   }
+  #   @client.execute(api_method: @service.events.insert,
+  #     parameters: {'calendarId': calendar_id,
+  #       'sendNotifications': true},
+  #     body: JSON.dump(event),
+  #     headers: {'Content-Type': 'application/json'}
+  #   )
+  # end
 
 end
